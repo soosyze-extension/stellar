c51959a97fba147c61f583725ffe7579eadb3979
noelma <noel-mathieu@hotmail.fr>
2019-03-07 19:35:03 +0100

    Initial commit.

---
+++ html.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<!--
+        Stellar by HTML5 UP
+        html5up.net | @ajlkn
+        Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+    <head>
+        <title><?php echo $title ?></title>
+        <?php if( $favicon ): ?>
+            <link rel="shortcut icon" type="image/png" href="<?php echo $favicon; ?>"/>
+        <?php endif; ?>
+        <meta charset="utf-8" />
+        <meta name="description" content="<?php echo $description ?>"/>
+        <meta name="keywords" content="<?php echo $keyboard ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="<?php echo $base_theme; ?>assets/css/main.css" />
+        <link rel="stylesheet" href="<?php echo $base_theme; ?>assets/css/layout.css" />
+        <link rel="stylesheet" href="<?php echo $base_theme; ?>assets/css/style.css" />
+        <noscript><link rel="stylesheet" href="<?php echo $base_theme; ?>assets/css/noscript.css" /></noscript>
+        <?php echo $styles ?>
+        <script src="<?php echo $base_theme; ?>assets/js/jquery.min.js"></script>
+    </head>
+    <body class="is-preload">
+        <!-- Wrapper -->
+        <div id="wrapper">
+            <?php echo $block[ 'page' ] ?>
+        </div>
+        <!-- Scripts -->
+        <script src="<?php echo $base_theme; ?>assets/js/jquery.scrollex.min.js"></script>
+        <script src="<?php echo $base_theme; ?>assets/js/jquery.scrolly.min.js"></script>
+        <script src="<?php echo $base_theme; ?>assets/js/browser.min.js"></script>
+        <script src="<?php echo $base_theme; ?>assets/js/breakpoints.min.js"></script>
+        <script src="<?php echo $base_theme; ?>assets/js/util.js"></script>
+        <script src="<?php echo $base_theme; ?>assets/js/main.js"></script>
+        <?php echo $scripts ?>
+    </body>
+</html>
---
+++ page-front.php
@@ -0,0 +1,55 @@
+<!-- Header -->
+<header id="header" class="alt">
+    <?php if( $logo ): ?>
+        <span class="logo">
+            <a href="<?php echo $base_path; ?>">
+                <img src="<?php echo $logo; ?>" alt="Logo site" class="img-responsive logo">
+            </a>
+        </span>
+    <?php endif; ?>
+    <h1><?php echo $title; ?></h1>
+    <p>Just another free, fully responsive site template.</p>
+</header>
+<!-- Nav -->
+<nav id="nav">
+    <?php echo $block[ 'main_menu' ]; ?>
+</nav>
+<!-- Main -->
+<div id="main">
+    <section id="intro" class="main">
+        <div class="spotlight">
+            <div class="content">
+                <header class="major">
+                    <h2><?php echo $title_main; ?></h2>
+                    <?php echo $block[ 'messages' ] ?>
+                </header>
+                <?php echo $block[ 'content' ] ?>
+            </div>
+        </div>
+    </section>
+</div>
+<!-- Footer -->
+<footer id="footer">
+    <section>
+        <h2>Aliquam sed mauris</h2>
+        <p>Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat 
+            magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. 
+            Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, 
+            vitae tincidunt odio.</p>
+    </section>
+    <section>
+        <h2>Etiam feugiat</h2>
+        <dl class="alt">
+            <dt>Address</dt>
+            <dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
+            <dt>Phone</dt>
+            <dd>(000) 000-0000 x 0000</dd>
+            <dt>Email</dt>
+            <dd><a href="#">information@untitled.tld</a></dd>
+        </dl>
+        <?php echo $block[ 'second_menu' ]; ?>
+    </section>
+    <p class="copyright">Power by <a href="http://soosyze.com/">SoosyzeCMS</a> |  
+        Built by <a href="https://twitter.com/ajlkn">@ajlkn</a> for <a href="https://html5up.net">HTML5 UP</a>.
+    </p>
+</footer>
